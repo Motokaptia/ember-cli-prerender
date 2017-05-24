1089d1ba5dd08ffe73d6e28f4227e247fbbdbcaa
Ardalan Naghshineh <ardalan.naghshineh@gmail.com>
2017-05-24 15:06:45 -0600

    Improved blueprint: Adding routes to router.js is now optional

--- blueprints/sitemap/index.js
+++ blueprints/sitemap/index.js
@@ -7,12 +7,29 @@ module.exports = {
   },
 
   afterInstall: function(options) {
-    return this.insertIntoFile('app/router.js', `  this.route('${options.entity.name}-txt', { path: '${options.entity.name}.txt' });` +
-          ` // Required by ember-cli-prerender\n` +
-          `  this.route('${options.entity.name}-xml', { path: '${options.entity.name}.xml' }); // Optional\n`, {
-        after: 'Router.map(function() {\n'
+    const routes = [
+      `  this.route('${options.entity.name}-txt', { path: '${options.entity.name}.txt' }); // Required by ember-cli-prerender`,
+      `\n  this.route('${options.entity.name}-xml', { path: '${options.entity.name}.xml' }); // Optional\n`,
+    ];
+
+    return this.ui.prompt({
+        type: 'list',
+        name: 'addRoutes',
+        message: `How would you like to add the following sitemaps to your routes?\n\n${routes.join('')}\n`,
+        choices: [
+          { name: 'Automatic: Add them for me.', value: true },
+          { name: 'Manual: I will add them myself.', value: false }
+        ]
       })
-      .then(() => this.ui.writeLine(`Added route ${options.entity.name}-txt to app/router.js`))
-      .then(() => this.ui.writeLine(`Added route ${options.entity.name}-xml to app/router.js`));
+      .then(({ addRoutes }) => {
+        if (addRoutes) {
+          return this.insertIntoFile('app/router.js', routes.join(''), {
+              after: 'Router.map(function() {\n'
+            })
+            .then(() => this.ui.writeLine(`Added route ${options.entity.name}-txt to app/router.js`))
+            .then(() => this.ui.writeLine(`Added route ${options.entity.name}-xml to app/router.js`));
+        }
+      })
+      .then(() => this.ui.writeLine(`Sitemap generation is done.`));
   },
 };
