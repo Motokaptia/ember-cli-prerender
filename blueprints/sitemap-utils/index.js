cc84aebab6af7f09f0c5ca053a80f9aacab8301b
Ardalan Naghshineh <ardalan.naghshineh@gmail.com>
2017-05-25 10:54:54 -0600

    Fixed: The `ember generate <entity-name>` command requires an entity name to be specified.

--- blueprints/sitemap-utils/index.js
+++ blueprints/sitemap-utils/index.js
@@ -1,4 +1,8 @@
 /* eslint-env node */
 module.exports = {
   description: 'Generate sample utility functions needed for sitemaps.',
+
+  normalizeEntityName: function(entityName){
+    return entityName || 'utility-functions';
+  },
 };
